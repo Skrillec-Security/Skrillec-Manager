0c999c3758e91d9530d71787239829acdd6502b0
Lamp07 <96823938+Lamp07@users.noreply.github.com>
2022-04-01 21:05:47 +0700

    Update start_api.py

--- core/api_server/start_api.py
+++ core/api_server/start_api.py
@@ -1,1 +1,11 @@
+from flask import Flask, jsonify, request
 
+# Creating Flask APP
+api = Flask(__name__)
+
+@api.route("/")
+def index():
+    return "Skrillec API Manager"
+
+def start_api():
+    api.run(host = "0.0.0.0", port = 80)
