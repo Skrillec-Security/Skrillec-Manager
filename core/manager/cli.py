2aaed6128c2ad1a1be4eba1178f4f8d292240df8
xvSync <71209712+xvSync@users.noreply.github.com>
2022-04-01 15:22:14 +0100

    Update cli.py

--- core/manager/cli.py
+++ core/manager/cli.py
@@ -3,6 +3,3 @@ class CLI:
     def start_skrillec() -> None:
         while True:
             UserInput = (str(input(" -> ")))
-            for x in range(0, len(CLI().Sensitive_Chars)):
-                if UserInput in CLI().Sensitive_Chars:  print("Please type valid arguments.")
-                else:   print("Carry on")
