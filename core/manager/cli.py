efa697021506e88bdcef22a67fc688766bf842eb
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-04-01 14:21:01 -0400

    Update cli.py

--- core/manager/cli.py
+++ core/manager/cli.py
@@ -2,6 +2,6 @@
 from cmd_handler import *
 
 def start_skrillec() -> None:
-        while True:
-            UserInput = (str(input(" -> ")))
+    while True:
+        UserInput = (str(input(" -> ")))
         CMD_Handle = cmd_handler(UserInput)
